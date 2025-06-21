1b084454b849f815d6fc301cc8a7d8561af9359d
Misha_Pobereznyi <Pober1997@gmail.com>
2025-06-21 20:44:52 +0300

    HW6 PageProps fixes

--- app/notes/[id]/page.tsx
+++ app/notes/[id]/page.tsx
@@ -2,17 +2,20 @@ import { dehydrate, HydrationBoundary, QueryClient } from "@tanstack/react-query
 import { fetchNoteById } from "@/lib/api";
 import NoteDetailsClient from "../NoteDetails.client";
 
-export default async function NoteDetailsPage({
-  params,
-}: {
-  params: { id: string };
-}) {
-  const id = Number(params.id);
+type NoteDetailsProps = {
+  params: Promise<{ id: string }>;
+};
+
+export default async function NoteDetailsPage({ params }: NoteDetailsProps) {
   const queryClient = new QueryClient();
 
+  const { id } = await params;
+  const parsedId = Number(id);
+
+
   await queryClient.prefetchQuery({
-    queryKey: ["note", id],
-    queryFn: () => fetchNoteById(id),
+    queryKey: ["note", parsedId],
+    queryFn: () => fetchNoteById(parsedId),
   });
 
   const dehydratedState = dehydrate(queryClient);
